db29e5f62f12ccd19359881fa6bfc4c5982e7b81
zhou <zhou773@foxmail.com>
2026-04-16 00:36:18 +0800

    feat: dataset table

--- llmops-api/internal/model/__init__.py
+++ llmops-api/internal/model/__init__.py
@@ -1,10 +1,18 @@
 
-from .app import App
+from .app import App, AppDatasetJoin
 from .api_tool import ApiTool, ApiToolProvider
 from .upload_file import UploadFile
+from .dataset import Dataset, Document, Segment, KeywordTable, ProcessRule, DatasetQuery
 __all__ = [
     "App",
+    "AppDatasetJoin",
     "ApiTool",
     "ApiToolProvider",
-    "UploadFile"
+    "UploadFile",
+    "Dataset",
+    "Document",
+    "Segment",
+    "KeywordTable",
+    "ProcessRule",
+    "DatasetQuery"
 ]
