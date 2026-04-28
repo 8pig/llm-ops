9133566fbc9b9ddfa18650fb85a998eaf311d2c7
zhou <zhou773@foxmail.com>
2026-04-28 19:24:22 +0800

    feat: TODO

--- llmops-api/internal/core/agent/agents/function_call_agent.py
+++ llmops-api/internal/core/agent/agents/function_call_agent.py
@@ -1,11 +1,12 @@
 from typing import Literal
 
-from langchain_core.messages import AnyMessage, HumanMessage
+from langchain_core.messages import AnyMessage, HumanMessage, SystemMessage, RemoveMessage
 from langgraph.constants import END
 from langgraph.graph.state import CompiledStateGraph, StateGraph
 
+from internal.exception import FailException
 from .base_agennt import BaseAgent
-from internal.core.agent.entities.agent_entity import AgentState
+from internal.core.agent.entities.agent_entity import AgentState, AGENT_SYSTEM_PROMPT_TEMPLATE
 
 
 class FunctionCallAgent(BaseAgent):
@@ -50,11 +51,51 @@ class FunctionCallAgent(BaseAgent):
 
     def _long_term_memory_recall_node(self, state: AgentState) -> AgentState:
         # 长期记忆召回
-        pass
+        # 根据传递的智能体配置判断是否需要召回长期记忆
+        long_term_memory = ""
+        if self.agent_config.enable_long_term_memory:
+            long_term_memory = state["long_term_memory"]
 
+        # 构建预设消息列表，并将preset_prompt+long_term_memory填充到系统消息中
+        preset_messages = [
+            SystemMessage(AGENT_SYSTEM_PROMPT_TEMPLATE.format(
+                preset_prompt=self.agent_config.preset_prompt,
+                long_term_memory=long_term_memory,
+            ))
+        ]
+
+        # 将短期历史消息添加到消息列表中
+        history = state["history"]
+        if isinstance(history, list) and len(history) > 0:
+            # 校验历史消息是不是复数形式，[人类消息, AI消息, 人类消息, AI消息, ...]
+            if len(history) % 2 != 0:
+                raise FailException("智能体历史消息列表格式错误")
+            # 拼接历史消息
+            preset_messages.extend(history)
+
+            # 因为
+            # LangChain
+            # 的消息对象是不可变的，而且消息的位置由添加到列表的顺序决定。所以必须：
+            # 删除旧位置的消息
+            # 在新位置添加新消息
+
+        # 拼接当前用户的提问信息
+        human_message = state["messages"][-1]
+        preset_messages.append(HumanMessage(human_message.content))
+
+        # 处理预设消息，将预设消息添加到用户消息前，先去删除用户的原始消息，然后补充一个新的代替
+        return {
+            "messages": [RemoveMessage(id=human_message.id), *preset_messages],
+        }
 
     def _llm_node(self, state: AgentState) -> AgentState:
         # 大模型节点
+        llm = self.agent_config.llm
+        # 判断llm 是否有bind_tools
+        if hasattr(llm, "bind_tools") and callable(llm.bind_tools) and len(self.agent_config.tools) > 0:
+            llm = llm.bind_tools(self.agent_config.tools)
+
+
 
         pass
 
