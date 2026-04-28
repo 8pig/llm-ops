683033a35d0e3b358785c4817d69f12714693254
zhou <zhou773@foxmail.com>
2026-04-28 11:02:46 +0800

    style:  减少脚本启动噪音

--- llmops-api/start_celery.ps1
+++ llmops-api/start_celery.ps1
@@ -1,6 +1,7 @@
 # start_celery.ps1
 
 $env:PYTHONPATH = $PSScriptRoot
+$env:PYTHONWARNINGS = "ignore::SyntaxWarning,ignore::DeprecationWarning,ignore::ResourceWarning,ignore::UserWarning"
 Get-ChildItem -Path D:\code\llm-ops\llmops-api -Filter *.pyc -Recurse -File | Remove-Item -Force
 Get-ChildItem -Path D:\code\llm-ops\llmops-api -Filter __pycache__ -Recurse -Directory | Remove-Item -Recurse -Force
 uv run celery -A app.http.app.celery worker -l info --pool eventlet
