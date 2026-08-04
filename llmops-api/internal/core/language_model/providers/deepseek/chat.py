0e8f4881cf13141da29a8b04ce837a8314c095f4
zhou <zhou773@foxmail.com>
2026-08-04 21:16:00 +0800

    feat:  ds qw provider

---
+++ llmops-api/internal/core/language_model/providers/deepseek/chat.py
@@ -0,0 +1,9 @@
+
+from langchain_openai import ChatOpenAI
+
+from internal.core.language_model.entities.model_entity import BaseLanguageModel
+
+
+class Chat(ChatOpenAI, BaseLanguageModel):
+    """deepseek聊天模型"""
+    pass
